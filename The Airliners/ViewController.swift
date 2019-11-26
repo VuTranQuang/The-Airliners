665b8bdf1e7b47feb9cbfe545c61f6fa1a2a7e69
VuTQ10 <vutq10@VuTQ10s-Mac-mini.local>
2019-11-26 15:41:41 +0700

    Initial Commit

---
+++ The Airliners/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  The Airliners
+//
+//  Created by VuTQ10 on 11/26/19.
+//  Copyright © 2019 VuTQ10. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
